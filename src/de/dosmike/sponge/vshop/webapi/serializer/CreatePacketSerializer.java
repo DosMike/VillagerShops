2703a85e8245a27316e2e8b470dacdaa7c906eba
DosMike <xanatosefan@gmx.de>
2017-11-18 14:25:47 +0100

    API-7
    Updating dependecies; Removing Causes where neccessary; Updating WebAPI
    to latest API 7

--- src/de/dosmike/sponge/vshop/webapi/serializer/CreatePacketSerializer.java
+++
@@ -1,7 +0,0 @@
-package de.dosmike.sponge.vshop.webapi.serializer;
-
-import de.dosmike.sponge.vshop.webapi.packets.CreatePacket;
-
-public class CreatePacketSerializer extends JsonSerializer<CreatePacket> {
-	
-}
